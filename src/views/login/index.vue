5f2cf32ac224b4d4876e566c748a5e07e6250409
chenbin <180910061@qq.com>
2021-07-02 17:31:29 +0800

    通用样式设置

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,9 @@
 <!--  -->
 <template>
   <div class="login">
-
+    <div class="login_title">
+      <h1 class="title">{{title}}</h1>
+    </div>
   </div>
 </template>
 
@@ -9,6 +11,7 @@
 export default {
   data () {
     return {
+      title:"xxxx管理系统"
     }
   }
 }
@@ -18,6 +21,18 @@ export default {
 .login{
   height: 100%;
   background: url("~@/assets/img/login_bg.jpg") center no-repeat;
-    background-size: cover;
+  background-size: cover;
 }
+.login_title{
+  height: 120px;
+ 
+}
+.title{
+  font-family: 'Microsoft YaHei';
+  line-height: 1em;
+  color: #fff;
+  font-weight: bold;
+  padding: 40px 0 0 50px;
+}
+
 </style>
