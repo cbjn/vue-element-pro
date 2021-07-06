a2c7c60851a2ba3514aa9b5f6a12f7f7c1d6ddbc
chenbin <180910061@qq.com>
2021-07-06 17:25:42 +0800

    升级vue3.0

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import store from '@/store'
+import { store } from '@/store'
 import router from '@/router'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
 import "assets/global.css"
-Vue.config.productionTip = false
 
-Vue.use(ElementUI)
-
-
-new Vue({
-    store,
-    router,
-    render: h => h(App),
-}).$mount('#app')
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
