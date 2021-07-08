611ab1983277ec252b1765260d1a8af57503553c
chenbin <180910061@qq.com>
2021-07-08 14:22:16 +0800

    登录样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,38 +1,53 @@
 <!--  -->
 <template>
   <div class="login">
-    <div class="login_title">
-      <h1 class="title">{{ title }}</h1>
-    </div>
-    <div class="loginBox">
-      <el-form ref="form" :model="form" :size="medium">
-        <el-form-item :required="true">
-          <el-input v-model="form.username" prefix-icon="el-icon-user-solid" placeholder="请输入用户名"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-input
-            v-model="form.password"
-            show-password
-            prefix-icon="el-icon-lock"
-            placeholder="请输入密码"
-          ></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-row>
-            <el-col :span="12">
-              <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
-            </el-col>
-            <el-col :span="12">
-              <div class="grid-content bg-purple-light"></div>
-            </el-col>
-          </el-row>
-        </el-form-item>
-        <el-form-item style="text-align:center">
-          <el-button type="primary">登录</el-button>
-          <el-button>取消</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
+    <el-container class="allHeight">
+      <el-header height="100px">
+        <div class="allHeight">
+          <p class="title">{{ title }}</p>
+        </div>
+      </el-header>
+      <el-main>
+        <div class="loginBox">
+          <el-form ref="form" :model="form" :size="medium">
+            <el-form-item :required="true">
+              <el-input
+                v-model="form.username"
+                prefix-icon="el-icon-user-solid"
+                placeholder="请输入用户名"
+              ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-input
+                v-model="form.password"
+                show-password
+                prefix-icon="el-icon-lock"
+                placeholder="请输入密码"
+              ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-row>
+                <el-col :span="12">
+                  <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
+                </el-col>
+                <el-col :span="12">
+                  <div class="grid-content bg-purple-light"></div>
+                </el-col>
+              </el-row>
+            </el-form-item>
+            <el-form-item style="text-align:center;">
+              <el-button type="primary">登录</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-main>
+      <el-footer>
+        <div class="foot">
+          <p id="copyright">Copyright © 2020.vue-element-pro.</p>
+        </div>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -59,21 +74,33 @@ export default {
   background-size: cover;
   position: relative;
 }
-.login_title {
-  height: 120px;
+.allHeight {
+  height: 100%;
 }
 .title {
   font-family: "Microsoft YaHei";
   line-height: 1em;
   color: #fff;
   font-weight: bold;
+  font-size: 3em;
   padding: 40px 0 0 50px;
 }
 .loginBox {
   height: 380px;
   width: 300px;
-  right: 300px;
-  top: 350px;
+  right: 320px;
+  top: 280px;
+  background: url("~@/assets/login/denglukuang01.png") center no-repeat;
   position: absolute;
+}
+.el-form {
+  margin: 60px 15px 15px 15px;
+}
+.el-form-item {
+  margin-bottom: 32px;
+}
+.foot {
+  color: #fff;
+  text-align: center;
 }
 </style>
